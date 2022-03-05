598d555f2e2bb551101977fea7aa89bcd1e89793
Elie az <elie@decentra-tech.com>
2022-03-05 17:05:00 +0200

    added swager / postgres properties

--- src/main/java/com/ULFGII/Mobility/Service/CountryService.java
+++ src/main/java/com/ULFGII/Mobility/Service/CountryService.java
@@ -3,10 +3,12 @@ package com.ULFGII.Mobility.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ULFGII.Mobility.DAO.ContryRepo;
 import com.ULFGII.Mobility.Entity.Country;
 
+@Service
 public class CountryService {
 
 	@Autowired
